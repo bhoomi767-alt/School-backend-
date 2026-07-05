332753d27f59570f3c8456a10f20b95da797d550
Bhoomi dubey <bhoomidubey444@gmail.com>
2026-07-05 12:43:46 +0530

    Backend

--- server.js
+++ server.js
@@ -890,7 +890,7 @@ mongoose.connect(mongoUri)
 //     console.log("Server running 3000");
 // });
 // ✅ Ise replace karein code ke bilkul niche:
-const PORT = process.env.BACKEND_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
