f90e1d10f05f21d5b46ff0a07520476dbe6a7e3d
Bhoomi dubey <bhoomidubey444@gmail.com>
2026-07-06 12:32:06 +0530

    Fix direct cloudinary path

--- server.js
+++ server.js
@@ -489,15 +489,18 @@ app.post("/api/admin/send-otp", async(req, res) => {
 
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
-            secure: true, // SSL
+            port: 587,
+            secure: false,
+            requireTLS: true,
 
             auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD,
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
             },
 
             family: 4,
+            connectionTimeout: 10000,
+            greetingTimeout: 10000,
         });
         console.log("SMTP OK");
 
